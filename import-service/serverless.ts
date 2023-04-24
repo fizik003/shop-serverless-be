1a63ee02fb16ad95b3596c4f75882193c919a050
Yury Barysevich1 <kassik0boks@gmail.com>
2023-04-24 14:02:36 +0300

    feat: added env to serverless

--- import-service/serverless.ts
+++ import-service/serverless.ts
@@ -30,6 +30,8 @@ const serverlessConfiguration: AWS = {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
       REGION: '${self:provider.region}',
+      UPLOAD_FOLDER: 'uploaded',
+      UPLOAD_BUCKET: 'import-service003',
     },
   },
   // import the function via paths
